477928a722e75e6aa7de13a79efd8bd43a1c0003
Mrhaltung <92170743+Mrhaltung@users.noreply.github.com>
2021-11-19 17:55:26 +0530

    Stock Account Management Program

---
+++ streamReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Stock_Account_Management
+{
+    internal class streamReader
+    {
+        private string path;
+
+        public streamReader(string path)
+        {
+            this.path = path;
+        }
+
+        internal string ReadToEnd()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
