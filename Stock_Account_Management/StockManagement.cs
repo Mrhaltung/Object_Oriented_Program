5fb7f1400d20725a30713004347629024ac7889e
Mrhaltung <92170743+Mrhaltung@users.noreply.github.com>
2021-11-19 18:02:42 +0530

    Stock Account Management Program

---
+++ Stock_Account_Management/StockManagement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stock_Account_Management
+{
+    public class StockManagement
+    {
+        public List<Stock> Stock;
+    }
+
+    public class Stock
+    {
+        public string StockName;
+        public int quatity;
+        public double price;
+    }
+}
